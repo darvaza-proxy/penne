28e64a3b770459cb18cd4efc9e4294f0fad73d2b
Alejandro Mery <amery@jpi.io>
2023-12-23 01:42:12 +0000

    server: skip horizons when answering CHAOS requests
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/server/server_resolver.go
+++ pkg/server/server_resolver.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"context"
+
 	"github.com/miekg/dns"
 
+	"darvaza.org/resolver/pkg/errors"
+	"darvaza.org/resolver/pkg/exdns"
 	"darvaza.org/slog"
 
 	"darvaza.org/penne/pkg/resolver"
@@ -33,5 +37,24 @@ func (*Server) reflectEnabled(_ string) (slog.LogLevel, bool) {
 
 // ServeDNS handles dns requests based on the IP address of the client
 func (srv *Server) ServeDNS(rw dns.ResponseWriter, req *dns.Msg) {
+	var chaos bool
+
+	exdns.ForEachQuestionOfClass(req, dns.ClassCHAOS, func(_ dns.Question) {
+		chaos = true
+	})
+
+	if chaos {
+		// route CHAOS requests directly to ExchangeCHAOS
+		ctx := context.Background()
+
+		rsp, err := srv.ExchangeCHAOS(ctx, req)
+		if err != nil {
+			rsp = errors.ErrorAsMsg(req, err)
+		}
+
+		_ = rw.WriteMsg(rsp)
+		return
+	}
+
 	srv.z.ServeDNS(rw, req)
 }
